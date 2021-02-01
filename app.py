545bea00d64e30bc293501a36bd379e0287f6510
Larry Volz <imaginologist@gmail.com>
2021-01-31 21:57:41 -0500

    board looks good

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+from flask import Flask, request, redirect, render_template
+from unittest import TestCase 
 from boggle import Boggle
 
+app=Flask(__name__)
+
+
 boggle_game = Boggle()
+
+
+@app.route('/')
+def home_instructions():
+    return render_template('index.html')
+
+@app.route('/game-start')
+def game_start():
+    return render_template('game-start.html')
